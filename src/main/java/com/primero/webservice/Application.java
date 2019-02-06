9693509ff7b24100294c823795f7b9efea4c9128
Y_K_Y <zzzzz.co.kr@gmail.com>
2019-02-07 02:36:05 +0900

    test

--- src/main/java/com/primero/webservice/Application.java
+++ src/main/java/com/primero/webservice/Application.java
@@ -2,14 +2,21 @@ package com.primero.webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+    public static final String APPLICATION_LOCATION = "spring.config.location="
+            + "classpath:application.yml,"
+            + "/app/config/spring-webservice/real-application.yml";
 
+    public static void main(String[] args){
+
+        new SpringApplicationBuilder(Application.class)
+                .properties(APPLICATION_LOCATION)
+                .run(args);
+    }
 }
--- src/main/java/com/primero/webservice/web/WebRestController.java
+++ src/main/java/com/primero/webservice/web/WebRestController.java
@@ -1,20 +1,22 @@
 package com.primero.webservice.web;
 
 import com.primero.webservice.service.PostsService;
-import com.primero.webservice.domain.posts.PostsRepository;
 import com.primero.webservice.web.dto.PostsSaveRequestDto;
 import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+
 @RestController
 @AllArgsConstructor
 public class WebRestController {
 
-    private PostsRepository postsRepository;
     private PostsService postsService;
+    private Environment env;
 
     @GetMapping("/hello")
     public String hello(){
@@ -23,4 +25,11 @@ public class WebRestController {
 
     @PostMapping("/posts")
     public void savePosts(@RequestBody PostsSaveRequestDto dto){ postsService.save(dto); }
+
+    @GetMapping("/profile")
+    public String getProfile() {
+        return Arrays.stream(env.getActiveProfiles())
+                .findFirst()
+                .orElse("");
+    }
 }
